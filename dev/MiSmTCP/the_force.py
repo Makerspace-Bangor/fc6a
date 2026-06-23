2be1019265ef5cae77673c270326b4d12cefde70
FOSSBOSS <blicken@gmail.com>
2026-06-22 21:59:38 -0400

    Testing the Force

--- dev/MiSmTCP/the_force.py
+++ dev/MiSmTCP/the_force.py
@@ -10,21 +10,26 @@ plc.release_force()
 #print("Q1 forced")
 """
 Normal output writes may have no effect if the 
-PLC program conflicts with those cpmmands 
+PLC program conflicts with those commands 
+
+pre run state: Q1, Q6 are on.
+
 """
 for i in range(8):
-    plc.output(f"Q{i}", 0)
+    plc.output(f"Q{i}", 1)
+sleep(5)
 for i in range(8):
-    plc.output(f"Q{i}", 1)    
+    plc.output(f"Q{i}", 0)    
 
 """ Force all low """    
 for i in range(8):    
     plc.force(f"Q{i}",0)
 
 print("Normal write has no effect because force IO is set")
-
+sleep(10)
 for i in range(8):
     plc.output(f"Q{i}", 0)
+sleep(3)    
 for i in range(8):
     plc.output(f"Q{i}", 1)    
 
@@ -35,7 +40,13 @@ for i in range(8):
 
 sleep(5)
 plc.write("Q1", 1) # lets see if the sigal to enable is retained. 
-"""
-pre run state: Q1, Q7 are on.
-"""
+
 plc.release_force()
+"""
+return to nominal program state.
+
+I think I kinda knew it before, but you can have both 
+the serial and TCP connections active concurently. 
+So from this program I can watch the Force state change in windLDR
+which I think is neat.
+"""
